17d3f72b94dd376924b80844bc3fa3bec082a20b
Festus Phillip <st.festus4cruise@gmail.com>
2026-06-08 05:04:48 +0100

    made selected works visible

--- pages/Awards.tsx
+++ pages/Awards.tsx
@@ -1,34 +1,34 @@
 import AwardsCard from "../components/AwardsCard";
 
 export type AWARDS = {
-    src: string;
-    alt: string;
+  src: string;
+  alt: string;
   text: string;
   date: string;
 };
 const Awards = () => {
   const awards: AWARDS[] = [
     {
-        src: "/award1.png",
-        alt: "bunner-stumn",
+      src: "/award1.png",
+      alt: "bunner-stumn",
       text: "the digital zone click of san tonia",
       date: "22-12-2002",
     },
     {
-        src: "/award2.png",
-        alt: "jullian",
+      src: "/award2.png",
+      alt: "jullian",
       text: "Berkely lounge of indiana stage",
       date: "12-03-2009",
     },
     {
-        src: "/award3.png",
-        alt: "nilter",
+      src: "/award3.png",
+      alt: "nilter",
       text: "Bellir of resont de noire of the central",
       date: "26-09-2014",
     },
     {
-        src: "/fashion4.png",
-        alt: "Akaila-Souza",
+      src: "/fashion4.png",
+      alt: "Akaila-Souza",
       text: "mira de mistra and art piecer",
       date: "12-12-2021",
     },
@@ -41,7 +41,12 @@ const Awards = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4 mt-10">
         {awards.map((item, index) => (
           <div key={index}>
-            <AwardsCard src={item.src} alt={item.alt} text={item.text} date={item.date} />
+            <AwardsCard
+              src={item.src}
+              alt={item.alt}
+              text={item.text}
+              date={item.date}
+            />
           </div>
         ))}
       </div>
